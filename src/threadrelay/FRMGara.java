a3650be88fe5d60717c3298f106ff423ab3c27dd
buccialex <bucci.alex@franchettisalviani.net>
2026-04-12 21:07:17 +0200

    eliminato main della form
    ne ho approfittato per chiedere a claude come poter aggiornare la grafica senza avere contaminazioni nella logica, mi è stato proposto l'utilizzo di un timer per la grafica, ritengo questa proposta come valida e meno invasiva di una contaminazione tra grafica e logica

--- src/threadrelay/FRMGara.java
+++ src/threadrelay/FRMGara.java
@@ -193,7 +193,6 @@ public class FRMGara extends javax.swing.JFrame {
         btnFerma.setEnabled(true);
 
         Corridore c;
-        Testimone test;
         Thread t;
 
         t = new Thread(c = new Corridore());
@@ -203,21 +202,7 @@ public class FRMGara extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btnAvviaActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new FRMGara().setVisible(true));
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAvvia;
